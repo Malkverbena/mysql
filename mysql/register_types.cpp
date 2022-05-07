ab4e2bd59ed66b8aff19e0865e1e920e7bfc47d2
Cleber Alves <malkverbena@gmail.com>
2022-05-07 19:49:57 -0300

    Add files via upload
    Support to Godot 3 & 4 build.

--- mysql/register_types.cpp
+++ mysql/register_types.cpp
@@ -3,12 +3,26 @@
 #include "register_types.h"
 #include "mysql.h"
 
-#ifdef GODOT4
-	#include "core/object/class_db.h"
-#else
-	#include "core/class_db.h"
-#endif
 
+#ifdef GODOT4
+#include "core/object/class_db.h"
+
+void initialize_mysql_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+	GDREGISTER_CLASS(MySQL);
+}
+
+void uninitialize_mysql_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
+
+
+#else
+#include "core/class_db.h"
 
 void register_mysql_types() {
 	ClassDB::register_class<MySQL>();
@@ -16,4 +30,6 @@ void register_mysql_types() {
 
 void unregister_mysql_types() {
 }
+
+#endif
 
