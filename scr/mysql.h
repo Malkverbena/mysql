8c69f583962b6fe766c83d6587634dc0a968b375
Cleber <malkverbena@gmail.com>
2024-02-26 12:18:14 -0300

    Added some notes for possible future new features.

--- scr/mysql.h
+++ scr/mysql.h
@@ -1,5 +1,14 @@
 /* mysql.h */
 
+
+/*
+	SOME NICE THINGS TO TAKE A LOOK:
+
+	Query formatting:
+	https://github.com/boostorg/mysql/pull/213#pullrequestreview-1890118842
+	
+*/
+
 #ifndef MYSQL_H
 #define MYSQL_H
 
